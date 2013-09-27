520858d6cc9ae7730c3834c572ddf288cc772e50
rafikime <rafael.teyral@gmail.com>
2013-09-27 12:14:50 +0200

    habe den fehler gefunden: verifyError: wenn die library nicht im libs/ ordner liegt sondern wo anders noclassdeffounderror: die rome-library musste auch in den classpath, war anscheinend nur im runtime path

--- src/com/example/rss/fragments/FeedListFragment.java
+++ src/com/example/rss/fragments/FeedListFragment.java
@@ -1,11 +1,17 @@
 package com.example.rss.fragments;
 
 
+import java.net.URL;
+
 import android.app.ListFragment;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.io.SyndFeedInput;
+import com.sun.syndication.io.XmlReader;
 
 public class FeedListFragment extends ListFragment {
 	
@@ -28,12 +34,19 @@ public class FeedListFragment extends ListFragment {
 			 lastPosition = savedInstanceState.getInt(STATE_KEY, 0);
 		 
 		 ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_activated_1);
-//		 RssReader reader = new RssReader();
-//		 RssFeed feed = reader.readFeed("http://rss.nytimes.com/services/xml/rss/nyt/Sports.xml");
-//		 System.out.println("read rss feed");
-//		 adapter.add(feed.getTitle());
-//		 adapter.add(feed.getLanguage());
-//		 FeedItem item = new FeedItem();
+		 //TODO use underlying code on other thread than ui-thread
+//		 try {
+//             URL feedUrl = new URL("http://rss.nytimes.com/services/xml/rss/nyt/Sports.xml");
+//
+//             SyndFeedInput input = new SyndFeedInput();
+//             SyndFeed feed = input.build(new XmlReader(feedUrl));
+//             System.out.println(feed.toString());
+//             adapter.add(feed.getAuthor() + " " + feed.getTitle());
+//         }
+//         catch (Exception ex) {
+//             ex.printStackTrace();
+//             System.out.println("ERROR: "+ex.getMessage());
+//         }
 		 
 		 
 		 adapter.add("click to go to details");
@@ -41,7 +54,6 @@ public class FeedListFragment extends ListFragment {
 		 adapter.add("cant add real feed-item :(");
 		 setListAdapter(adapter);
 		 
-		 //TODO set 
 	 }
 	 
 	 @Override
