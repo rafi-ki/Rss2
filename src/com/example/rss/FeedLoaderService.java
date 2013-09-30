cc2c4e36f45511138d71f487a9b36c81c48a5ae5
rafikime <rafael.teyral@gmail.com>
2013-09-30 10:41:08 +0200

    changed service to intentservice

--- src/com/example/rss/FeedLoaderService.java
+++ src/com/example/rss/FeedLoaderService.java
@@ -3,7 +3,7 @@ package com.example.rss;
 import java.net.URL;
 import java.util.LinkedList;
 
-import android.app.Service;
+import android.app.IntentService;
 import android.content.Intent;
 import android.os.IBinder;
 import android.widget.Toast;
@@ -12,61 +12,50 @@ import com.sun.syndication.feed.synd.SyndFeed;
 import com.sun.syndication.io.SyndFeedInput;
 import com.sun.syndication.io.XmlReader;
 
-public class FeedLoaderService extends Service {
+public class FeedLoaderService extends IntentService {
+	
 	
 	FeedManager feedManager;
-
-	@Override
-	public void onCreate() {
+	
+	public FeedLoaderService() {
+		super("FeedLoaderService");
 		feedManager = FeedManager.getInstance();
 	}
-
-	@Override
-	public IBinder onBind(Intent arg0) {
-		return null;
-	}
+	
 	
 	@Override
-	public int onStartCommand(Intent intent, int flags, int startId)
-	{
-		loadFeeds();
-		return START_STICKY;
-	}
-	
-	public void loadFeeds()
-	{
-		Thread t = new Thread()
-		{
-			public void run()
-			{
-				feedManager.restoreSubscribedFeeds();
-				LinkedList<Feed> feeds = feedManager.getFeedlist();
-				for (Feed feed : feeds)
-				{
-					try{
-						//TODO do it the right way
-						URL feedUrl = new URL(feed.getFeedurl());
-						SyndFeedInput input = new SyndFeedInput();
-		                SyndFeed synFeed = input.build(new XmlReader(feedUrl));
-		                
-//		                Intent intent = new Intent("receive feeds");
-//		                intent.putExtra("feed", synFeed.getLink());
-//		                LocalBroadcastManager.getInstance(getBaseContext()).sendBroadcast(intent); // send broadcast
-					}
-	                catch (Exception ex) {
-		                ex.printStackTrace();
-		                System.out.println("ERROR: "+ex.getMessage());
-		            }
-	            }
-			}
-		};
-		t.start();
+	public int onStartCommand(Intent intent, int flags, int startId) {
+	    System.out.println("loading feeds");
+	    return super.onStartCommand(intent,flags,startId);
 	}
 	
 	@Override
 	public void onDestroy() {
-		Toast.makeText(this, "service done", Toast.LENGTH_SHORT).show();
+		System.out.println("feeds load destroyed");
 		super.onDestroy();
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		feedManager.restoreSubscribedFeeds();
+		LinkedList<Feed> feeds = feedManager.getFeedlist();
+		for (Feed feed : feeds)
+		{
+			try{
+				//TODO do it the right way
+				URL feedUrl = new URL(feed.getFeedurl());
+				SyndFeedInput input = new SyndFeedInput();
+                SyndFeed synFeed = input.build(new XmlReader(feedUrl));
+                
+//                Intent intent = new Intent("receive feeds");
+//                intent.putExtra("feed", synFeed.getLink());
+//                LocalBroadcastManager.getInstance(getBaseContext()).sendBroadcast(intent); // send broadcast
+			}
+            catch (Exception ex) {
+                ex.printStackTrace();
+                System.out.println("ERROR: "+ex.getMessage());
+            }
+        }
 	}
 	
 	
--- src/com/example/rss/parser/RssReader.java
+++ src/com/example/rss/parser/RssReader.java
@@ -1,112 +1,137 @@
 package com.example.rss.parser;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.Characters;
+import javax.xml.stream.events.XMLEvent;
+
+import com.example.rss.model.FeedItem;
 import com.example.rss.model.RssFeed;
 
-public class RssReader
-{
-    private URL url;
+public class RssReader {
+  static final String TITLE = "title";
+  static final String DESCRIPTION = "description";
+  static final String CHANNEL = "channel";
+  static final String LANGUAGE = "language";
+  static final String COPYRIGHT = "copyright";
+  static final String LINK = "link";
+  static final String AUTHOR = "author";
+  static final String ITEM = "item";
+  static final String PUB_DATE = "pubDate";
+  static final String GUID = "guid";
 
-    private static final String TITLE = "title";
-    private static final String DESCRIPTION = "description";
-    private static final String LANGUAGE = "language";
-    private static final String LINK = "link";
-    private static final String AUTHOR = "author";
-    private static final String ITEM = "item";
-    private static final String DATE = "pubDate";
-    private static final String FEED_ID = "guid";
-    
+  final URL url;
 
-    public RssFeed readFeed(String strURL)
-    {
-        if (strURL == null) //can not be parsed
-            return null;
-
-        try {
-			this.url = new URL(strURL);
-		} catch (MalformedURLException mue) {
-			mue.printStackTrace();
-		}
-        RssFeed feed = null;
-        
-//        boolean isFeedHeader = true;
-//        String author = "";
-//        String title = "";
-//        String language = "";
-//        String description = "";
-//        String feedId = "";
-//        String link = "";
-//        String date = "";
-//
-//        try
-//        {
-//            // First create a new XMLInputFactory
-//            XMLInputFactory inputFactory = XMLInputFactory.newInstance();
-//            // Setup a new eventReader
-//            InputStream in = url.openStream();
-//            XMLEventReader eventReader = inputFactory.createXMLEventReader(in);
-//            // Read the XML document
-//            while (eventReader.hasNext()) {
-//                XMLEvent event = eventReader.nextEvent();
-//                if (event.isStartElement())
-//                {
-//                    String localPart = event.asStartElement().getName().getLocalPart();
-//                    if (localPart.equals(ITEM))
-//                    {
-//                    	if (isFeedHeader)
-//                    	{
-//	                    	isFeedHeader = false;
-//	                    	feed = new RssFeed(title, link, description, language, date);
-//                    	}
-//                    } 
-//                    else if (localPart.equals(TITLE))
-//                    	title = getStringFromReader(event, eventReader);
-//                    else if (localPart.equals(DESCRIPTION))
-//                    	description = getStringFromReader(event, eventReader);
-//                    else if (localPart.equals(LANGUAGE))
-//                    	language = getStringFromReader(event, eventReader);
-//                    else if (localPart.equals(LINK))
-//                    	link = getStringFromReader(event, eventReader);
-//                    else if (localPart.equals(AUTHOR))
-//                    	author = getStringFromReader(event, eventReader);
-//                    else if (localPart.equals(DATE))
-//                    	date = getStringFromReader(event, eventReader);
-//                    else if (localPart.equals(FEED_ID))
-//                    	feedId = getStringFromReader(event, eventReader);
-//                }
-//                else if (event.isEndElement())
-//                {
-//                    if (event.asEndElement().getName().getLocalPart().equals(ITEM))
-//                    {
-//                        FeedItem feedItem = new FeedItem();
-//                        feedItem.setAuthor(author);
-//                        feedItem.setDescription(description);
-//                        feedItem.setFeedId(feedId);
-//                        feedItem.setLink(link);
-//                        feedItem.setTitle(title);
-//                        feed.addFeedItem(feedItem);
-//                    }
-//                }
-//            }
-//        } catch (XMLStreamException e)
-//        {
-//            throw new RuntimeException(e);
-//        } catch (IOException e) {
-//			throw new RuntimeException(e);
-//		}
-
-        return feed;
+  public RssReader(String feedUrl) {
+    try {
+      this.url = new URL(feedUrl);
+    } catch (MalformedURLException e) {
+      throw new RuntimeException(e);
     }
-    
-//    private String getStringFromReader(XMLEvent event, XMLEventReader eventReader)
-//    	      throws XMLStreamException {
-//    	    String result = "";
-//    	    event = eventReader.nextEvent();
-//    	    if (event instanceof Characters) {
-//    	      result = event.asCharacters().getData();
-//    	    }
-//    	    return result;
-//    }
-}
+  }
+
+  public RssFeed readFeed() {
+    RssFeed feed = null;
+    try {
+      boolean isFeedHeader = true;
+      // Set header values intial to the empty string
+      String description = "";
+      String title = "";
+      String link = "";
+      String language = "";
+      String author = "";
+      String pubdate = "";
+      String guid = "";
+
+      // First create a new XMLInputFactory
+      XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+      // Setup a new eventReader
+      InputStream in = read();
+      XMLEventReader eventReader = inputFactory.createXMLEventReader(in);
+      // Read the XML document
+      while (eventReader.hasNext()) {
+        XMLEvent event = eventReader.nextEvent();
+        if (event.isStartElement()) {
+          String localPart = event.asStartElement().getName()
+              .getLocalPart();
+          	if (localPart.equals(ITEM))
+          	{
+	            if (isFeedHeader) {
+	              isFeedHeader = false;
+	              feed = new RssFeed(title, link, description, language,
+	                  pubdate);
+	            }
+	            event = eventReader.nextEvent();
+          	}
+          	else if (localPart.equals(TITLE))
+          	{
+          		title = getCharacterData(event, eventReader);
+          	}
+          	else if (localPart.equals(DESCRIPTION))
+          	{
+          		description = getCharacterData(event, eventReader);
+          	}
+          	else if (localPart.equals(LINK))
+          	{
+          		link = getCharacterData(event, eventReader);
+          	}
+          	else if (localPart.equals(GUID))
+          	{
+          		guid = getCharacterData(event, eventReader);
+          	}
+          	else if (localPart.equals(LANGUAGE))
+          	{
+          		language = getCharacterData(event, eventReader);
+          	}
+          	else if (localPart.equals(AUTHOR))
+          	{
+          		author = getCharacterData(event, eventReader);
+          	}
+          	else if (localPart.equals(PUB_DATE))
+          	{
+          		pubdate = getCharacterData(event, eventReader);
+          	}
+        } else if (event.isEndElement()) {
+          if (event.asEndElement().getName().getLocalPart() == (ITEM)) {
+            FeedItem message = new FeedItem();
+            message.setAuthor(author);
+            message.setDescription(description);
+            message.setFeedId(guid);
+            message.setLink(link);
+            message.setTitle(title);
+            feed.addFeedItem(message);
+            event = eventReader.nextEvent();
+            continue;
+          }
+        }
+      }
+    } catch (XMLStreamException e) {
+      throw new RuntimeException(e);
+    }
+    return feed;
+  }
+
+  private String getCharacterData(XMLEvent event, XMLEventReader eventReader)
+      throws XMLStreamException {
+    String result = "";
+    event = eventReader.nextEvent();
+    if (event instanceof Characters) {
+      result = event.asCharacters().getData();
+    }
+    return result;
+  }
+
+  private InputStream read() {
+    try {
+      return url.openStream();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+} 
