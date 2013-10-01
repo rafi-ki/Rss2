26d528a1ffa4889f1be1f60a277c512bb25326d5
rafikime <rafael.teyral@gmail.com>
2013-10-01 21:54:34 +0200

    listview_item hinzugefügt, besteht aus title, link und publicDate

--- src/com/example/rss/FeedLoaderService.java
+++ src/com/example/rss/FeedLoaderService.java
@@ -40,14 +40,11 @@ public class FeedLoaderService extends IntentService {
 
 	@Override
 	protected void onHandleIntent(Intent intent) {
-		feedManager.restoreSubscribedFeeds();
 		LinkedList<FeedLink> feeds = feedManager.getFeedlist();
 		System.out.println("loading feeds " + feeds.size());
 		for (FeedLink feed : feeds)
 		{
 			try{
-				URL feedUrl = new URL(feed.getFeedurl());
-			
 				//custom rss reader
 				CustomRssReader reader = CustomRssReader.getInstance();
 				RssFeed rss = reader.read(feed.getFeedurl());
@@ -59,27 +56,4 @@ public class FeedLoaderService extends IntentService {
             }
         }
 	}
-	
-	private final class RssHandler extends Handler {
-		/**
-		 * {@inheritDoc}
-		 */
-		@Override
-		public void handleMessage(Message msg) {
-			super.handleMessage(msg);
-			if (msg.what == RssReader.ID) {
-				System.out.println("right id");
-				if (msg.arg1 == ServiceProcessor.RETURN_MESSAGE_OK) {
-					String[] items = (String[]) msg.obj;
-					System.out.println("handler is called with " + items.length + " items");
-					for (String item : items)
-						System.out.println(item);
-				}
-			}
-			
-		}
-	}
-	
-
-	
 }
--- src/com/example/rss/fragments/FeedListFragment.java
+++ src/com/example/rss/fragments/FeedListFragment.java
@@ -3,6 +3,11 @@ package com.example.rss.fragments;
 
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -14,12 +19,13 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.actionbarsherlock.app.SherlockListFragment;
 import com.example.rss.FeedLink;
 import com.example.rss.FeedLoaderService;
-import com.example.rss.SubscribedFeedManager;
 import com.example.rss.R;
+import com.example.rss.SubscribedFeedManager;
 
 public class FeedListFragment extends SherlockListFragment {
 	
@@ -52,14 +58,33 @@ public class FeedListFragment extends SherlockListFragment {
 		 if (savedInstanceState != null) // save state
 			 lastPosition = savedInstanceState.getInt(STATE_KEY, 0);
 		 
-		 ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_activated_1);
-		 
-		 for(FeedLink f: feedmanager.getFeedlist()){
-			 adapter.add(f.getFeedurl());
-		 }
+//		 ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_activated_1);
+		 List<Map<String, String>> list = getFeedData();
+		 SimpleAdapter adapter = new SimpleAdapter(getActivity(), list,
+					R.layout.subscribed_lv_item, new String[] { "title", "link",
+							"date" }, new int[] { R.id.subscribed_lv_item_title,
+							R.id.subscribed_lv_item_link, R.id.subscribed_lv_item_date }
+			);
 		 
 		 setListAdapter(adapter);
 		 
+	 }
+	 
+	 //TODO return empty list if ready
+	 private List<Map<String, String>> getFeedData()
+	 {
+		 List<Map<String, String>> list = new ArrayList<Map<String, String>>();
+		 Map map = new HashMap<String, String>();
+		 map.put("title", "Feed Title1");
+		 map.put("link", "http://blablaliej.com");
+		 map.put("date", "12.02.2013");
+		 list.add(map);
+		 map = new HashMap<String, String>();
+		 map.put("title", "Feed Title2");
+		 map.put("link", "http:///hurra.com");
+		 map.put("date", "30.01.2011");
+		 list.add(map);
+		 return list;
 	 }
 	 
 	 @Override
