d6989041a73ad89218180b4dc259c90423756ac5
Dominik Schildorfer <dominik.schildorfer@gmail.com>
2013-10-02 20:23:50 +0200

    icon in action bar not displayed in subscribe fragment

--- src/com/example/rss/fragments/SubscriberFragment.java
+++ src/com/example/rss/fragments/SubscriberFragment.java
@@ -2,6 +2,9 @@ package com.example.rss.fragments;
 
 
 import com.actionbarsherlock.app.SherlockFragment;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
 import com.example.rss.R;
 
 import android.os.Bundle;
@@ -10,14 +13,16 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class SubscriberFragment extends SherlockFragment {
-
+	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-		
-		
+		setHasOptionsMenu(true);
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.subscribe_to_feed_fragment, container, false);
     }
-
+	@Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.removeItem(R.id.action_subscribe);
+    }
 }
