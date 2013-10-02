9ae823b0ab732fbf1fa5701062d2837469d4b22f
Dominik Schildorfer <dominik.schildorfer@gmail.com>
2013-10-02 20:54:39 +0200

    initial work done to change the subscribe to feed click event to use broadcasts

--- src/com/example/rss/fragments/SubscriberFragment.java
+++ src/com/example/rss/fragments/SubscriberFragment.java
@@ -7,12 +7,29 @@ import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
 import com.example.rss.R;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 
 public class SubscriberFragment extends SherlockFragment {
+	
+/*	@Override
+	public void onStart(){
+		super.onStart();
+		Button button = (Button) getView().findViewById(R.id.subscribe_ok_button);
+		button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				//hide keyboard
+				InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+				imm.hideSoftInputFromWindow(getView().findViewById(R.id.subscribe_url_input).getWindowToken(), 0);
+			}
+		});
+
+	}*/
 	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
